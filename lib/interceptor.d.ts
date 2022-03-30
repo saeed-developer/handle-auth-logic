46263bc85f1bf37f6176a6f790b34eb1d7a20e5c
saeed-developer <saeednajafimedia@gmail.com>
2022-03-30 21:55:36 +0430

    delete cached compiled file

--- lib/interceptor.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Interceptor {
-    reqeustToken: any;
-    axios?: any;
-    errorCode?: number;
-}
-export declare const interceptor: ({ reqeustToken, axios, errorCode, }: Interceptor) => void;
--- lib/localStorage.d.ts
+++
@@ -1,8 +0,0 @@
-interface Token {
-    tokenKeyName: string;
-    tokenValue: string;
-    refreshValue: string;
-}
-export declare const setItem: ({ tokenKeyName, tokenValue, refreshValue, }: Token) => void;
-export declare const getItem: (tokenKeyName?: string) => any;
-export {};
